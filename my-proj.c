9c331f302fca51388281e3ecc88aeaef47216265
allinthemind66 <r.nilon92@gmail.com>
2018-08-25 18:07:04 -0400

    first commit

---
+++ my-proj.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char c[] = "C言語";
+	char ruby[] = "ルビー";
+	
+
+	printf("私は%sを理解していません\n", c);
+	printf("私は%sがより好きです\n", ruby);
+
+	return 0;
+}
